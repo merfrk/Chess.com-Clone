1d6002a2658a26f03da38a557b5145b8cc566eff
merfrk <o.farukartan@hotmail.com>
2025-09-11 09:40:05 +0300

    Initial Commit

---
+++ ChessClone/ChessCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessCloneApp.swift
+//  ChessClone
+//
+//  Created by Omer on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChessCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
